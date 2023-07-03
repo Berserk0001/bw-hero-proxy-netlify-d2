7384ebbeadde7eb69791536c70ff43119676f1ab
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2023-07-03 11:16:27 +0530

    Update shouldCompress.js

--- util/shouldCompress.js
+++ util/shouldCompress.js
@@ -1,5 +1,5 @@
-const MIN_COMPRESS_LENGTH = 512; // ~1Kb
-const MIN_TRANSPARENT_COMPRESS_LENGTH = MIN_COMPRESS_LENGTH * 2; // ~10KB
+const MIN_COMPRESS_LENGTH = 1024; // ~1Kb
+const MIN_TRANSPARENT_COMPRESS_LENGTH = MIN_COMPRESS_LENGTH * 10; // ~10KB
 
 function shouldCompress(originType, originSize, webp) {
 	if (!originType.startsWith("image")) return false;
