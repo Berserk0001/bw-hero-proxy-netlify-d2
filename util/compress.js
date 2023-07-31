f66e8487ab9301f179cb54f7ba113993762914b5
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2023-07-31 18:20:13 +0530

    Update compress.js

--- util/compress.js
+++ util/compress.js
@@ -10,7 +10,8 @@ function compress(input, webp, grayscale, quality, originSize) {
 		.grayscale(grayscale)
 		.toFormat(format, {
 			quality: quality,
-			effort: 1
+			effort: 1,
+			chromaSubsampling: '4:2:0'
 		})
 		.toBuffer({resolveWithObject: true})
 		.then(({data: output, info}) => {	// this way we can also get the info about output image, like height, width
